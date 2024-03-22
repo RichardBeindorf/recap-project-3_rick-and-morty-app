b2474d2171bfae38e97b509326f3925e92514de3
Haydar Algün <haydar.alguen@gmail.com>
2024-03-22 16:02:23 +0100

    testversion 1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import CharacterCard from "./components/CharacterCard/CharacterCard.js";
+import NavPagination from "./components/NavPagination/NavPagination.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
@@ -11,17 +12,20 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
 
+console.log("Start Page:", page);
 
-async function getCharacters() {
+async function getCharacters(page) {
   try {
-    const response = await fetch("https://rickandmortyapi.com/api/character/");
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/?page=${page}`
+    );
     const data = await response.json();
     console.log(data);
-    return data.results;
+    return data;
   } catch {
     console.log("API Fehler");
   }
@@ -30,7 +34,25 @@ async function getCharacters() {
 const allCharacters = await getCharacters();
 console.log(allCharacters);
 
-allCharacters.forEach((character) => {
+// getCharacter. info
+const pages = allCharacters.info;
+
+allCharacters.results.forEach((character) => {
   const newCharacter = CharacterCard(character);
   cardContainer.appendChild(newCharacter);
 });
+
+// By adding the string ?page=<pageIndex> to the end of the fetch URL, you can receive the respective page of characters.
+
+nextButton.addEventListener("click", () => {
+  page++;
+  maxPage = pages.pages;
+  console.log("Event Next Button:", pages);
+  const initialPage = 1;
+
+  return NavPagination(page, maxPage);
+});
+
+function newPage() {
+  return page + 1;
+}
