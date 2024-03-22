e3a8dff80a36720907e66c7d86771485f81b62bf
Haydar Algün <haydar.alguen@gmail.com>
2024-03-22 11:47:37 +0100

    first try

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,1 +1,29 @@
-export function CharacterCard() {}
+export default function CharacterCard({ src, name, status, type, occurrence }) {
+  const cardContainer = document.querySelector('[data-js="card-container"]');
+  const liCard = document.createElement("li");
+  liCard.setAttribute("data-js", "card");
+  liCard.classList.add("card");
+  liCard.innerHTML = `
+          <div class="card__image-container">
+            <img
+              class="card__image"
+              src=${src}
+              alt=${name}
+            />
+            <div class="card__image-gradient"></div>
+          </div>
+          <div class="card__content">
+            <h2 class="card__title">${name}</h2>
+            <dl class="card__info">
+              <dt class="card__info-title">Status</dt>
+              <dd class="card__info-description">${status}</dd>
+              <dt class="card__info-title">Type</dt>
+              <dd class="card__info-description">${type}</dd>
+              <dt class="card__info-title">Occurrences</dt>
+              <dd class="card__info-description">${occurrence}</dd>
+            </dl>
+          </div>
+    `;
+
+  return liCard;
+}
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import CharacterCard from "./components/CharacterCard/CharacterCard.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,11 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+testCharakter = {
+  src: "test",
+};
+
+const newCard = CharacterCard();
+
+cardContainer.append(newCard);
